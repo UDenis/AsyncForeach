7cab489f0407de6dbcdfd455535765517b6862be
Denis Maslov <denphobos@gmail.com>
2014-02-03 17:48:02 +0600

    first commit

---
+++ asyncForeach.js
@@ -0,0 +1,63 @@
+window.asyncForeach = (function() {
+
+	var toString = {}.toString,
+		noop = function() {},
+		objKeys;
+
+	objKeys = Object.keys || function(obj) {
+		var keys = [];
+		for (var k in obj) {
+			keys.push(k);
+		};
+		return keys;
+	};
+
+	function isArray(obj) {
+		return toString.apply(obj) == '[object Array]';
+	};
+
+	function isObject(obj) {
+		return obj != null && typeof obj == 'object';
+	};
+
+	function isFunction(value) {
+		return typeof value == 'function';
+	};
+
+	function asyncForeachForObj(obj, iterator, done) {
+		var keys = objKeys(obj);
+		if (keys.length > 0) {
+			asyncForeachForArray(keys, function(key, index, _done) {
+				iterator(obj[key], key, _done);
+			}, done);
+		}
+	};
+
+	function asyncForeachForArray(array, iterator, done) {
+		var currentIndex = -1,
+			length = array.length,
+			toNext;
+
+		toNext = function() {
+			currentIndex++;
+			if (currentIndex < length)
+				iterator(array[currentIndex], currentIndex, toNext);
+			else
+				done();
+		};
+		toNext();
+	};
+
+	function asyncForeach(obj, iterator, done) {
+		if (!isFunction(iterator))
+			throw "iterator parameter is not a function";
+
+		if (isArray(obj) && obj.length > 0) {
+			asyncForeachForArray(obj, iterator, done || noop);
+		} else if (isObject(obj)) {
+			asyncForeachForObj(obj, iterator, done || noop);
+		}
+	};
+
+	return asyncForeach;
+})();
---
+++ asyncForeachTest.js
@@ -0,0 +1,189 @@
+// Async foreach test
+(function() {
+
+	var each = asyncForeach;
+
+	// Usage example
+	each([1, 2, 3], function(item, index, done) {
+		setTimeout(function() {
+			console.log(item);
+			done();
+		}, 2000);
+	}, function() {
+		console.log("END");
+	});
+
+	asyncTest("Тестируем на массиве", function() {
+		expect(2);
+
+		var arrayCopy = [],
+			indexList = [],
+			iterator,
+			done,
+			array;
+
+		iterator = function(item, index, done) {
+			setTimeout(function() {
+				arrayCopy.push(item);
+				indexList.push(index);
+				done();
+			}, 100)
+		};
+
+		done = function() {
+			deepEqual(array, arrayCopy, "копирование массива прошло");
+			deepEqual(indexList, [0, 1, 2, 3], "копирование индексов массива прошло");
+			start();
+		};
+
+		array = [11, 22, 33, 44];
+		each(array, iterator, done);
+		this.clock.tick(1000);
+	});
+
+	asyncTest("Тестируем на объекте", function() {
+		expect(2);
+
+		var objCopy = {},
+			indexList = [],
+			iterator,
+			done,
+			obj;
+
+		iterator = function(item, index, done) {
+			setTimeout(function() {
+				objCopy[index] = item;
+				indexList.push(index);
+				done();
+			}, 100)
+		};
+
+		done = function() {
+			deepEqual(obj, objCopy, "копирование объекта прошло");
+			deepEqual(indexList, ['key1', 'key2', 'key3', 'key4'], "копирование ключей объекта прошло");
+			start();
+		};
+
+		obj = {
+			key1: 11,
+			key2: 22,
+			key3: 33,
+			key4: 44
+		};
+
+		each(obj, iterator, done);
+		this.clock.tick(1000);
+	});
+
+	asyncTest("Тестируем на асинхронность", function() {
+		expect(6);
+
+		//var array = {11,22,33,44];
+		var obj = {
+			key1: 11,
+			key2: 22,
+			key3: 33,
+			key4: 44
+		},
+			iterator,
+			doneSpy,
+			iteratorSpy,
+			asyncIterator;
+
+		iterator = function(item, intex, done) {
+			setTimeout(function() {
+				asyncIterator();
+				done();
+			}, 100);
+		};
+	
+		doneSpy = this.spy();
+		iteratorSpy = this.spy(iterator);
+		asyncIterator = this.spy();
+
+		each(obj, iteratorSpy, doneSpy);
+
+		ok(!doneSpy.called, "цикл не завершен")
+		ok(iteratorSpy.calledOnce, "цикл инициализировался");
+		ok(!asyncIterator.called, "цикл не начался");
+
+		this.clock.tick(1000);
+
+		ok(doneSpy.calledOnce, "цикл завершен");
+		equal(iteratorSpy.callCount, 4, "цикл завершился");
+		equal(asyncIterator.callCount, 4, "цикл завершился");
+
+		start();
+	});
+
+	asyncTest("Тестируем на пустом массиве, пустом объекте и на некорректном объекте и ф-ии итератора", function() {
+		expect(35);
+
+		var self = this,
+			testForEmpty = function(obj) {
+				var doneSpy = self.spy(),
+					iteratorSpy = self.spy(iterator),
+					asyncIterator = self.spy(),
+					iterator;
+
+				iterator = function(item, intex, done) {
+					setTimeout(function() {
+						asyncIterator();
+						done();
+					}, 100);
+				};
+
+				each(obj, iteratorSpy, doneSpy);
+
+				ok(!doneSpy.called, "цикл и не начался");
+				ok(!iteratorSpy.called, "цикл и не начался");
+				ok(!asyncIterator.called, "цикл и не начался");
+
+				self.clock.tick(1000);
+
+				ok(!doneSpy.called, "цикл и не начинался");
+				ok(!iteratorSpy.called, "цикл и не начинался");
+				ok(!asyncIterator.called, "цикл и не начинался");
+			},
+
+			testWithoutDone = function() {
+				var doneSpy = self.spy(),
+					asyncIterator = self.spy(),
+					iteratorSpy,
+					iterator;
+
+				iterator = function(item, intex, done) {
+					setTimeout(function() {
+						asyncIterator();
+						done();
+					}, 100);
+				};
+
+				iteratorSpy = self.spy(iterator);
+
+				each([1], iteratorSpy);
+
+				ok(iteratorSpy.called, "цикл инициализировался");
+
+				self.clock.tick(1000);
+
+				ok(!doneSpy.called, "цикл и не вызывался");
+				ok(iteratorSpy.calledOnce, "цикл и не начинался");
+				ok(asyncIterator.calledOnce, "цикл и не начинался");
+			};
+
+		testForEmpty([]);
+		testForEmpty({})
+		testForEmpty(null)
+		testForEmpty()
+		testForEmpty("")
+
+		testWithoutDone();
+
+		throws(function() {
+			each([1]);
+		}, "exception for not function occurred");
+		start();
+
+	});
+})()
